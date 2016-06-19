47b4b78bdd3d006c2c1d89269eefe32d0e641507
Raj Kumar <rajkum@thoughtworks.com>
2016-06-19 12:33:48 +0530

    Duplicate counts the text duplicates on the standard input

--- cla.go
+++ cla.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "./lib"
 
 func main() {
 	fmt.Println("Hello, world")
+	duplicate.Duplicate()
 }
---
+++ lib/duplicate.go
@@ -0,0 +1,20 @@
+package duplicate
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//Duplicate counts the text duplicates on the standard input
+func Duplicate() {
+	textMap := make(map[string]int)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "exit" {
+			break
+		}
+		textMap[scanner.Text()]++
+	}
+	fmt.Println(textMap)
+}
